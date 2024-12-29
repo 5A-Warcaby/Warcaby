07a4119d1fc6e4bb6836f10d13aeb62a2e847b26
ArtiXuXM <arturcibail05@gmail.com>
2024-12-29 14:53:23 +0100

    Update Game.cs to add board and place pieces on it

--- Warcaby/Warcaby/Game.cs
+++ Warcaby/Warcaby/Game.cs
@@ -4,6 +4,55 @@ namespace Warcaby
 {
      public void PlayGame()
      { 
-        //logika gry
+          public const int Size = 8;
+          public char[,] board;
+          public const char Empty = '.';
+          public const char White = 'w';
+          public const char Black = 'b';
+          public const char WhiteKing = 'W';
+          public const char BlackKing = 'B';
+          public char this[int row, int col]
+          {
+              get { return board[row, col]; }
+              set { board[row, col] = value; }
+          }
+          
+          public Board()
+          {
+              board = new char[Size, Size];
+              InitializeBoard();
+          }
+          
+          private void InitializeBoard()
+          {
+              for (int i = 0; i < Size; i++)
+                  for (int j = 0; j < Size; j++)
+                      board[i, j] = (i + j) % 2 == 0 ? ' ' : Empty;
+          
+              for (int i = 0; i < 3; i++)
+                  for (int j = 0; j < Size; j++)
+                      if ((i + j) % 2 != 0)
+                          board[i, j] = Black;
+          
+              for (int i = 5; i < Size; i++)
+                  for (int j = 0; j < Size; j++)
+                      if ((i + j) % 2 != 0)
+                          board[i, j] = White;
+          
+          }
+          
+          public void DisplayBoard()
+          {
+              Console.Clear();
+              Console.WriteLine("  A B C D E F G H");
+              for (int i = 0; i < Size; i++)
+              {
+                  Console.Write(8 - i + " ");
+                  for (int j = 0; j < Size; j++)
+                      Console.Write(board[i, j] + " ");
+                  Console.WriteLine();
+              }
+              Console.WriteLine("  A B C D E F G H");
+          }
      }
 }
